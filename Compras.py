32b34126129c0f08d747672ee3426714779d3a34
Andre Jeitani <86528356+andrejeitani@users.noreply.github.com>
2024-05-20 12:11:04 -0300

    Update Compras.py

--- Compras.py
+++ Compras.py
@@ -78,7 +78,6 @@ try:
         st.dataframe(filtered_df , use_container_width=True)
     
         a = len(filtered_df)
-        b = filtered_df['Total'].sum().round(2)
         st.write('Produtos para comprar: ',str(a) ,
             ' - ' ,
             'Total de intelbras a comprar: R$',
