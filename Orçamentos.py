6dab79a78e0923f4b778bae4e1c05fc1c6429eec
Andre Jeitani <86528356+andrejeitani@users.noreply.github.com>
2024-05-20 09:39:15 -0300

    Update Orçamentos.py

--- Orçamentos.py
+++ Orçamentos.py
@@ -80,7 +80,6 @@ try:
     #Informe de Quantitativos
     st.write("Total Orçado R$" ,total_real , grouping=True)
     st.write("Clientes atendidos:",str(len(clientes)))
-    st.write("A Taxa de conversão é de: ",tx_conversao)
     st.divider()
 
     #Define colunas de filtro
