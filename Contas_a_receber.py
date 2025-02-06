382d168f58396bc9fed58599106a2e3d258c34a9
Andre Jeitani <86528356+andrejeitani@users.noreply.github.com>
2025-02-06 17:26:30 -0300

    Update Contas_a_receber.py

--- Contas_a_receber.py
+++ Contas_a_receber.py
@@ -8,13 +8,13 @@ st.set_page_config(layout='wide',
                    page_title='SP Distribuidora - Contas a Receber em Aberto')
 
 try:
-    #Realiza o upload do arquivo e realiza os devidos tratamentos
+    #Realiza o upload dos arquivos
     arquivo_sicoob = st.file_uploader('Faça o Upload do Arquivo do Sicoob em Excel')
     sicoob = pd.read_excel(arquivo_sicoob)
     arquivo_santander = st.file_uploader('Faça o Upload do Arquivo do Santander em Excel')
     santander = pd.read_excel(arquivo_santander)
 
-    # Realiza a tratativa do Arquivo do Sicoob em Excel
+    # Renomeia as Colunas
     sicoob = sicoob.rename(columns={
         'Unnamed: 0':'Excluir1','Unnamed: 1':'Cliente','Unnamed: 2':'Excluir2','Unnamed: 3':'Excluir3', 'Unnamed: 4':'Excluir4',
         'Unnamed: 5':'Excluir5','Unnamed: 6':'Excluir6','Unnamed: 7':'Excluir7','Unnamed: 8':'Nosso Numero','Unnamed: 9':'Excluir9',
@@ -23,17 +23,19 @@ try:
         'Unnamed: 20':'Vencimento','Unnamed: 21':'Excluir21','Unnamed: 22':'Excluir22','Unnamed: 23':'Excluir23','Unnamed: 24':'Excluir24',
         'Unnamed: 25':'Excluir25','Unnamed: 26':'Valor', 'Unnamed: 27':'Excluir27'        
     })
+    # Excluir as Colunas Desnecessárias
     sicoob = sicoob.drop(columns=[
         'Excluir1','Excluir2', 'Excluir3','Excluir4','Excluir5','Excluir6','Excluir7','Excluir9','Excluir10','Excluir11',
         'Excluir13','Excluir14','Excluir15','Excluir16','Excluir17','Excluir18','Excluir19','Excluir21','Excluir22',
         'Excluir23','Excluir24','Excluir25','Excluir27'
     ])
+    # Apaga as células vázias (NA) e o Cabeçalho da planilha (Index == 'sacado') e inclui o nome do banco
     sicoob = sicoob.dropna()
     Excluir_index = sicoob[sicoob['Cliente'] == 'Sacado'].index
     sicoob = sicoob.drop(index=Excluir_index)
     sicoob['Banco'] = 'Sicoob'
 
-    # Realiza a Tratativa do Arquivo do Santander em Excel (XLSX)
+    # Excluir as celulas vazias (NA) , Exclui o Cabeçalho  , renomeia e excluir Colunas e inclui o nome do banco
     santander = santander.dropna()
     santander = santander.drop(index=5)
     santander = santander.rename(columns={'Cod. Beneficiário':'Seu Numero',
@@ -48,12 +50,13 @@ try:
     santander = santander[['Cliente','Nosso Numero','Seu Numero','Vencimento','Valor']]
     santander['Banco'] = 'Santander'
 
-    # Imprimir a Tabela Final Formatada
+    # Junta as tabelas dos 2 bancos e traz em formato final (formatada)
     tabela_final = pd.concat([sicoob,santander])         
     tabela_final = tabela_final.sort_values(by='Cliente' , ascending=True)
     tabela_final['Valor'] = tabela_final['Valor'].replace('.',',')
     tabela_final['Valor'] = tabela_final['Valor'].round(2)
-            
+
+    # Cria os campos e os filtros para pesquisar Clientes e/ou nota fiscais e imprimi a tabela filtrada
     def filtro_cliente():
         coluna1,coluna2 = st.columns(2)
         with coluna1:
@@ -73,18 +76,17 @@ try:
     filtro_cliente() 
 
 
-    # Imprimi o total agrupado por Cliente
+    # Agrupa a tabela por clientes e traz a quantidade de clientes inadimplentes
     st.title('Total Em Aberto Por Cliente')
     total_agregado_por_cliente = tabela_final.drop(columns=['Nosso Numero', 'Seu Numero', 'Vencimento','Banco'])
     total_agregado_por_cliente = total_agregado_por_cliente.groupby(by='Cliente').sum()
     total_agregado_por_cliente['%'] = (total_agregado_por_cliente['Valor'] / total_agregado_por_cliente['Valor'].sum() * 100)
     total_agregado_por_cliente = total_agregado_por_cliente.sort_values(by='%' , ascending=False)
     st.dataframe(total_agregado_por_cliente, use_container_width=True)
-
     devedores = len(tabela_final['Cliente'].unique())
     st.info(f'Existe um total de {devedores} Clientes em atraso')
 
-    # Imprimi agrupado por banco
+    # Agrupa a tabela por Banco e traz o total em aberto
     st.title('Total Em Aberto Por Banco')
     total_agregado_por_banco = tabela_final.drop(columns=['Nosso Numero', 'Seu Numero', 'Vencimento','Cliente'])
     total_agregado_por_banco = total_agregado_por_banco.groupby('Banco').sum()
@@ -93,6 +95,7 @@ try:
     total_agregado_por_banco_sem_perc = total_agregado_por_banco['Valor']
     st.dataframe(total_agregado_por_banco_sem_perc , use_container_width=True )
 
+    # Cria e imprimi um grafico de pizza com a representação percentual dos boletos em aberto por banco
     total_agregado_por_banco = total_agregado_por_banco.reset_index()
     grafico = px.pie(total_agregado_por_banco, values='%',
                         labels='Banco', 
@@ -100,6 +103,5 @@ try:
                         names='Banco',
                         color='Banco')
     st.plotly_chart(grafico)
-    
-except:
+except: # Evita mostrar codigos de erros ao usuario final, caso existe algum erro , apresenta o texto "SP Distribuidora"
     st.text('SP Distribudiora')
