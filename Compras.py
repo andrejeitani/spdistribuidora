c7b131bc34232b8a16eb6888df6738154937c4ca
Andre Jeitani <86528356+andrejeitani@users.noreply.github.com>
2025-02-21 14:47:36 -0300

    Update Compras.py

--- Compras.py
+++ Compras.py
@@ -46,9 +46,9 @@ try:
     df = df[df['Marca'] != 'LINEAR']
     Filtro_descricao = df['Produto'].str.contains('LICENCA')
     df = df[df['Produto'] != Filtro_descricao]
-    lista_de_codigos = ['4400338','4543509','4543516','4565298','4565299','4565329','4565330','4780051']
+    #lista_de_codigos = ['4400338','4543509','4543516','4565298','4565299','4565329','4565330','4780051']
     df = df.reset_index()
-    df = df[df['Codigo'] != lista_de_codigos]
+    #df = df[df['Codigo'] != lista_de_codigos]
 
     # Define o tipo de variavel em cada coluna
     df['Sugestao 40 Dias'] = df['Sugestao 40 Dias'].astype(float)
