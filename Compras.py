22e293cad1aa1daff02aae64460164ce6631266c
Andre Jeitani <86528356+andrejeitani@users.noreply.github.com>
2025-02-21 14:59:52 -0300

    Update Compras.py

--- Compras.py
+++ Compras.py
@@ -46,7 +46,7 @@ try:
     df = df[df['Marca'] != 'LINEAR']
     Filtro_descricao = df['Produto'].str.contains('LICENCA')
     df = df[df['Produto'] != Filtro_descricao]
-    df = df.reset_index()
+    #df = df.reset_index()
     #codigos = [4400338,4543509,4543516,4565298,4565299,4565329,4565330,4780051]
     #lista = df['Codigo'].str.contains(codigos)
     #df = df[df['Codigo'] != codigos]
