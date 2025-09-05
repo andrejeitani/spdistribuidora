1c089083ac7afb6bacd1b210d2b7fc11b65e282c
Andre Jeitani <86528356+andrejeitani@users.noreply.github.com>
2025-09-05 08:25:37 -0300

    Update Compras.py

--- Compras.py
+++ Compras.py
@@ -90,14 +90,12 @@ try:
     df = df[df['Codigo'] != '4750146'] # AP 3000 AX
     df = df[df['Codigo'] != '4564045'] # VIP 5500 FISH EYE
     df = df[df['Codigo'] != '4681027'] # LE 170
-    #df = df[df['Codigo'] != '4681020'] # LE 130
-    #df = df[df['Codigo'] != '4681021'] # LE 130 MF
-    #df = df[df['Codigo'] != '4710018'] # 1200 A ADAPTADOR WIFI
     df = df[df['Codigo'] != '4760040'] # SF 900 HI POE
     df = df[df['Codigo'] != '4780039'] # VEX 3004
     df = df[df['Codigo'] != '4780072'] # VEX 3120
     df = df[df['Codigo'] != '4780073'] # VEX 3120
     df = df[df['Codigo'] != '4810040'] # VB 503
+    df = df[df['Codigo'] != '4682070'] # CT 3000 2PB
     df = df[df['Codigo'] != '4580787'] # NVD 1416 # SUBSTITUIDO PELO 1516
     df = df[df['Codigo'] != '4580934'] # NVD 3316 P # SUBSTITUIDO PELO iNVD 3016 P
     df = df[df['Codigo'] != '4842905'] # MODULO FOTOVOLTAICO EMS 170
