bfa864b386634ba7644c60dcb2728fb8b323177c
Andre Jeitani <86528356+andrejeitani@users.noreply.github.com>
2025-12-05 10:04:09 -0300

    Update Compras.py

--- Compras.py
+++ Compras.py
@@ -551,9 +551,8 @@ try:
     ##############################################################################################
     # Itens em promoção
     promocao = ['4540055','4541073','4543544','4543590','4541041','4541032','4540089','4540088','4541076','4541014','4550018','4541019']
-    #df['Produto'] = np.where(
-    np.where(df['Codigo'].isin(promocao) , df['Produto'] = 'Item em Promoção' , df['Produto'])
-    #)
+    descricao = 'Item em Campanha'
+    df['Produto'] = np.where(df['Codigo'].isin(promocao) , df['Produto'] = descricao , df['Produto'])
     ##############################################################################################
   
     # Define o tipo de variavel em cada coluna
